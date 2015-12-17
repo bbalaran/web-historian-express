da35bc1d4df4333254dbd31fd627c8c94385cf7c
bbalaran <balaran202@gmail.com>
2015-12-16 20:16:23 -0800

    added bodyparser and morgan

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,9 +1,15 @@
 var http = require("http");
 var express = require("express");
 var path = require('path');
+var morgan = require ('morgan');
+var bodyParser = require('body-parser');
+
 var app = express();
 
+app.use(morgan('dev'));
 app.use(express.static('client'));
+app.use(bodyParser.urlencoded({extended : true}))
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 
