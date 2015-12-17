36610c450de32ccc08c2d3ef140c8f1667fc6c30
bbalaran <balaran202@gmail.com>
2015-12-16 20:22:03 -0800

    server sending back objects with sample sites

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,23 +1,40 @@
 var http = require("http");
 var express = require("express");
 var path = require('path');
-var morgan = require ('morgan');
+var morgan = require('morgan');
 var bodyParser = require('body-parser');
 
 var app = express();
+//temporary storage
+var sites = {
+  'google': {
+    queued: false,
+    site: 'www.google.com',
+    html: ''
+  },
+
+  'walmart': {
+    queued: false,
+    site: 'www.walmart.com',
+    html: ''
+  }
+
+};
 
 app.use(morgan('dev'));
 app.use(express.static('client'));
-app.use(bodyParser.urlencoded({extended : true}))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-
+app.get('/:sites', function(req, res) {
+  res.json(sites)
 
 
 });
 
 var port = 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   return console.log('listening on http://localhost' + port);
 });
